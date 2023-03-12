fe40881b62272fa7a2f0d5da33e3ddb99f747573
perez1187 <o.perez1187@gmail.com>
2023-03-12 18:10:24 +0100

    common app

--- xyz_api/settings/base.py
+++ xyz_api/settings/base.py
@@ -33,6 +33,7 @@ THIRD_PARTY_APPS = [ ]
 LOCAL_APPS = [
     "core_apps.users",
     "core_apps.profiles",
+    "core_apps.common",
  ] 
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS

9fa15718179ef8e7ffd7fadf698a06db42224ccc
perez1187 <o.perez1187@gmail.com>
2023-03-12 17:54:46 +0100

    default settings, and def profiles, user app

---
+++ xyz_api/settings/local.py
@@ -0,0 +1,15 @@
+
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = "django-insecure-#pfedrtux#rv*5w2c%0a^-97qo#t_ma2tgcl2mr42t7m5u(qb#"
+# ALLOWED_HOSTS = []
+
+
+from .base import *
+from .base import env
+DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env(
+    "DJANGO_SECRET_KEY",
+    default="VoQE5G62Qu1Sk8cmBMa8V8D4nYhWazjaEoH9p9wWGPF4Pv23A3M68Wtme2BpHSwt",
+)
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
